e14fb50909867414cd420833d291203cb441ef53
Lance <lmreineer@gmail.com>
2022-11-28 23:32:55 +1000

    Adjust stylings

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 function changeSize(amount) {
     const gridContainer = document.getElementById('main');
     gridContainer.style.display = 'grid'
-    gridContainer.style.gridTemplateColumns = `repeat(${amount}, 1fr)`
-    gridContainer.style.gridTemplateRows = `repeat(${amount}, 1fr)`
+    gridContainer.style.gridTemplateColumns = `repeat(68, 1fr)`
+    gridContainer.style.gridTemplateRows = `repeat(68, 1fr)`
 
-    for(let i = 0; i < amount*amount; i++) {
+    for(let i = 0; i < 68*68; i++) {
         const grids = document.createElement('div');
-        grids.style.border = '1px solid gray'
+            grids.addEventListener('click', () => {
+                grids.style.backgroundColor = colorPicker.value;
+            })
         gridContainer.appendChild(grids);
     }
 }
 
+const colorPicker = document.getElementById('colorPicker');
+const colorMode = document.getElementById('colorMode');
+const rainbowMode = document.getElementById('rainbowMode');
+const toggleSquares = document.getElementById('toggleSquares');
+const rangeInput = document.getElementById('rangeInput');
+const eraseMode = document.getElementById('eraseMode');
+const clearMode = document.getElementById('clearMode');
+
+colorPicker.addEventListener('input', (e) => {
+    e.target.style.backgroundColor = colorPicker.value;
+    colorPicker.style.backgroundColor = 'transparent'
+});
+
+// function colorMode?
+
+
+window.onload = (changeSize);
