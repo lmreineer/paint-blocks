dbb6dc42256995066fa897e798efe5194426f40d
Lance <lmreineer@gmail.com>
2022-11-29 10:17:45 +1000

    Fix range slider

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 function changeSize(amount) {
     const gridContainer = document.getElementById('main');
     gridContainer.style.display = 'grid'
-    gridContainer.style.gridTemplateColumns = `repeat(68, 1fr)`
-    gridContainer.style.gridTemplateRows = `repeat(68, 1fr)`
+    gridContainer.style.gridTemplateColumns = `repeat(${amount}, 1fr)`
+    gridContainer.style.gridTemplateRows = `repeat(${amount}, 1fr)`
 
-    for(let i = 0; i < 68*68; i++) {
+    for(let i = 0; i < amount * amount; i++) {
         const grids = document.createElement('div');
-            grids.addEventListener('click', () => {
-                grids.style.backgroundColor = colorPicker.value;
-            })
+        grids.style.border = '1px solid gainsboro'
         gridContainer.appendChild(grids);
     }
 }
+
 
 const colorPicker = document.getElementById('colorPicker');
 const colorMode = document.getElementById('colorMode');
 const rainbowMode = document.getElementById('rainbowMode');
 const toggleSquares = document.getElementById('toggleSquares');
-const rangeInput = document.getElementById('rangeInput');
 const eraseMode = document.getElementById('eraseMode');
 const clearMode = document.getElementById('clearMode');
 
@@ -26,7 +24,6 @@ colorPicker.addEventListener('input', (e) => {
     colorPicker.style.backgroundColor = 'transparent'
 });
 
-// function colorMode?
+console.log(rangeInput.value)
 
-
-window.onload = (changeSize);
+window.onload = (changeSize(rangeInput.value));
