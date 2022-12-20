cd44ca1d04cf1e2195d0621ee3ea70386460c105
Lance <lmreineer@gmail.com>
2022-12-20 15:28:54 +1000

    Format files and organize folders

--- src/functions.js
+++ src/functions.js
@@ -1,27 +1,26 @@
 // Drop down section below:
-const defaultChoice = document.querySelector(".default");
-const selectionsContainer = document.querySelector(".selections-container");
-const selection = document.querySelectorAll(".selection");
+const defaultChoice = document.querySelector('.default');
+const selectionsContainer = document.querySelector('.selections-container');
+const selection = document.querySelectorAll('.selection');
 
-defaultChoice.addEventListener("click", () => {
-    selectionsContainer.classList.toggle("active");
-    selectionsContainer.style.visibility = 'visible'
+defaultChoice.addEventListener('click', () => {
+    selectionsContainer.classList.toggle('active');
+    selectionsContainer.style.visibility = 'visible';
 });
 
 defaultChoice.addEventListener('blur', () => {
     selectionsContainer.classList.remove('active');
-    selectionsContainer.style.visibility = 'hidden'
-})
+    selectionsContainer.style.visibility = 'hidden';
+});
 
-selection.forEach(x => {
-    x.addEventListener("click", (e) => {
+selection.forEach((x) => {
+    x.addEventListener('click', (e) => {
         defaultChoice.innerHTML = e.target.textContent;
         selectionsContainer.classList.remove('active');
-        selectionsContainer.style.visibility = 'hidden'
+        selectionsContainer.style.visibility = 'hidden';
     });
 });
-//Drop down section above
-
+// Drop down section above
 
 // let lastActive = selection[0];
 // selectionsContainer.addEventListener('click', (e) => {
@@ -29,12 +28,11 @@ selection.forEach(x => {
 //     if(!currentSelection) return;
 
 //     const wasActive = currentSelection.classList.contains('remove-others');
-    
+
 //     lastActive.classList.remove('remove-others');
 //     currentSelection.classList.toggle('remove-others', !wasActive);
 //     lastActive = currentSelection;
 // });
-
 
 const gridContainer = document.getElementById('main');
 const colorPicker = document.getElementById('colorPicker');
@@ -42,7 +40,6 @@ const colorMode = document.getElementById('colorMode');
 const randomMode = document.getElementById('randomMode');
 const eraseMode = document.getElementById('eraseMode');
 const clearMode = document.getElementById('clearMode');
-
 
 let mouseIsDown = false;
 gridContainer.addEventListener('mousedown', (e) => {
@@ -56,75 +53,71 @@ gridContainer.addEventListener('mouseup', () => {
 });
 
 gridContainer.addEventListener('mouseover', (e) => {
-    if(mouseIsDown) {
+    if (mouseIsDown) {
         activateColor(e);
         activateErase(e);
     }
 });
 
-colorMode.style.filter = 'invert(100%)'
+colorMode.style.filter = 'invert(100%)';
 colorMode.addEventListener('click', () => {
     randomMode.classList.remove('random-active');
     eraseMode.classList.remove('erase-active');
-    colorMode.style.filter = 'invert(100%)'
+    colorMode.style.filter = 'invert(100%)';
     removeFilter(randomMode); removeFilter(eraseMode);
 });
 
 randomMode.addEventListener('click', (e) => {
     randomMode.classList.add('random-active');
     eraseMode.classList.remove('erase-active');
-    randomMode.style.filter = 'invert(100%)'
+    randomMode.style.filter = 'invert(100%)';
     removeFilter(colorMode); removeFilter(eraseMode);
 });
 
 eraseMode.addEventListener('click', () => {
     randomMode.classList.remove('random-active');
     eraseMode.classList.add('erase-active');
-    eraseMode.style.filter = 'invert(100%)'
+    eraseMode.style.filter = 'invert(100%)';
     removeFilter(colorMode); removeFilter(randomMode);
 });
 
 function activateColor(e) {
-    if(randomMode.classList.contains('random-active')) {
+    if (randomMode.classList.contains('random-active')) {
         const color = randomColor();
-        e.target.style.backgroundColor = '#' + color;
+        e.target.style.backgroundColor = `#${color}`;
         e.preventDefault();
-    }
-    else if(!randomMode.classList.contains('random-active')) {
+    } else if (!randomMode.classList.contains('random-active')) {
         e.target.style.backgroundColor = colorPicker.value;
         e.preventDefault();
     }
-};
+}
 
 function randomColor() {
     return Math.floor(Math.random() * 16777215).toString(16);
-};
+}
 
 function activateErase(e) {
-    if(eraseMode.classList.contains('erase-active')) {
-        e.target.style.backgroundColor = ''
+    if (eraseMode.classList.contains('erase-active')) {
+        e.target.style.backgroundColor = '';
         e.preventDefault();
-    }
-    else {
-        return; 
     }
 }
 
 function clearCanvas(grids) {
-    grids.style.backgroundColor = ''
+    grids.style.backgroundColor = '';
 }
 
 function removeFilter(mode) {
     mode.style.removeProperty('filter');
-};
+}
 
 function changeSize(size) {
     const squares = document.getElementById('toggleSquares');
-    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`
-    
-    for(let i = 0; i < size * size; i++) {
-        const grids = document.createElement('div')
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < size * size; i++) {
+        const grids = document.createElement('div');
         grids.classList.add('canvas');
         gridContainer.appendChild(grids);
 
@@ -132,12 +125,12 @@ function changeSize(size) {
             clearCanvas(grids);
         });
 
-        selection.forEach(x => {
+        selection.forEach((x) => {
             x.addEventListener('click', () => {
                 clearCanvas(grids);
             });
         });
-    };
-};
+    }
+}
 
 window.onload = changeSize(64);
